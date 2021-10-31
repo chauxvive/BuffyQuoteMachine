a0eaa4a3a615dd57d74a5ea34f1f1fecdb14b660
Mary Paul <mpauldesigns@gmail.com>
2021-10-31 18:27:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -55,6 +55,12 @@ var quotes = [
 
     "You know, I honestly don't think there's a human word fabulous enough for me.<br>- Glory",
     
+    "It's horrible! That's me as a vampire? I'm so evil and skanky. And I think I'm kind of gay.<br>- Willow",
+    
+    "I've seen honest faces before. They usually come attached to liars.<br>- Willow",
+    
+    "No. Bunch of wanna blessed be's. Nowadays every girl with a henna tattoo and a spice rack thinks she's a sister to the dark ones.<br>- Willow",
+    
     "The Big Moments are going to come. You can't help that. It's what you do afterwards that counts. That's when you find out who you are. <br>- Buffy",
     
      "Strong is fighting. It's hard and it's painful and it's every day. It's what we have to do and we can do it together, but if you're too much of a coward for that, then burn. <br>- Buffy",
